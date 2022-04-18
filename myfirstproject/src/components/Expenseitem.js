393d178ced8f5e3a61491b08445497fceeb6ca25
saklen <saklenp.albiorix@gmail.com>
2022-04-18 18:25:30 +0530

    component and props example

---
+++ myfirstproject/src/components/Expenseitem.js
@@ -0,0 +1,22 @@
+import './Expenseitem.css' 
+
+function Expenseitem(props) {
+    
+
+    return (
+    <div className="expense-item">
+        <div>
+          {props.Date.toISOString( )}
+        </div>
+        <div className="expense-item__description">
+            <h2>
+                {props.title}
+            </h2>
+    <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </div>
+    
+    );
+}
+
+export default Expenseitem;
