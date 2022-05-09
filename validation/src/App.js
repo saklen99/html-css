3f85ef15dbc554a144e359d53786a6e2ebb47a11
DESKTOP-RIVEPTH\Admin <saklenp.albiorix@gmail.com>
2022-05-09 20:21:52 +0530

    validation & adding user list component (section 8)

--- validation/src/App.js
+++ validation/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import AddUser from './components/Users/AddUser';
-
+import UsersList from './components/Users/UsersList';
 function App() {
+  const[us,uss]=useState([]);
+  const add=(u,n)=>
+  {
+    uss((pre)=>{
+      return[...pre,{name:u,age:n}];
+    })
+  }
   return (
     <div>
-      <AddUser />
+      <AddUser onAdduser={add} />
+      <UsersList users = {us} />
     </div>
   );
 }
--- validation/src/components/Users/AddUser.js
+++ validation/src/components/Users/AddUser.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 
 const  AddUser = (props) =>
 {
+    const [entername , setname] = useState('');
+    const [enterage , setage] = useState('');
     const Handler = (event) =>
     {
+       
         event.preventDefault();
+
+        if(entername.trim().length===0 || enterage.trim().length==0)
+        {
+            return;
+        }
+        if(+enterage < 1)
+        {
+            return;
+        }
+        props.onAdduser(entername,enterage);
+        setage('');
+        setname('');
+        
+
+    };
+
+    const uname = (event) =>{
+        setname(event.target.value);
+    };
+
+    const uage = (event) =>{
+        setage(event.target.value);
     };
 
     return(
         <Card className={classes.input}>
         <form onSubmit={Handler}>
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" />
+            <input type="text" id="username" onChange={uname} value={entername}/>
             <label htmlFor="age">Age:</label>
-            <input type="number" id="age" />
+            <input type="number" id="age" onChange={uage} value={enterage} />
             <button type="submit">Adduser</button>
         </form>
         </Card>
